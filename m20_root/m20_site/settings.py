b4f0820a8c4b14119fa5d0126423d214eb850ad4
Michael Bird <michaelbirdtx@protonmail.com>
2021-09-17 19:55:10 -0500

    Updated settings for deployment.

--- m20_root/m20_site/settings.py
+++ m20_root/m20_site/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = '/home/dh_uc592u/m20.michaelbirdtx.com/public/static/'
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
