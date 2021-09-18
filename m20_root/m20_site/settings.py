bb4e838eb6064ca5def749b9d18ca1b98c74b31b
Michael Bird <michaelbirdtx@protonmail.com>
2021-09-17 20:01:47 -0500

    Updated database path for deployment.

--- m20_root/m20_site/settings.py
+++ m20_root/m20_site/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'm20_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
